dabbf3f80b2e687cba3f6348a82a2bd61d09fd13
Wallstreet49 <wallstreet49@Wallstreet49s-MacBook-Pro.local>
2024-08-14 12:41:08 +0530

    done

--- src/components/FetchButton.tsx
+++ src/components/FetchButton.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import useFetchData from '../useFetchData';
 
 const FetchButton: React.FC = () => {
-  // const { data, isLoading, error, refreshData } = useFetchData('https://jsonplaceholder.typicode.com/todos');
+  const { data, isLoading, error, refreshData } = useFetchData('https://jsonplaceholder.typicode.com/todos');
 
   const handleClick = () => {
     refreshData(); // Call refreshData instead of fetchData to allow URL updating if necessary
@@ -14,8 +14,8 @@ const FetchButton: React.FC = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
-      <button onClick={handleClick}>Fetch Data</button>
+    <div className="text-center w-full h-full">
+      <button className="bg-black text-white px-4 py-2 rounded text-center" onClick={handleClick}>Fetch Data</button>
       {data.map((todo) => (
         <div key={todo.id}>
           <p>{todo.title}</p>
